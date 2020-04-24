713352b302dfa9d56a0227f937cabec020c658aa
i.ustinchenko <delfa07@bigmir.net>
2020-04-24 14:45:21 +0300

    03. js config

--- src/setupProxy.js
+++
@@ -1,8 +0,0 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
-module.exports = (app) => {
-  app.use([], createProxyMiddleware({
-    // target: process.env.PROXY_TARGET,
-    changeOrigin: true
-  }));
-};
