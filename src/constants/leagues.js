9784bfedf51edde5abd0312badab5861f4883780
i.ustinchenko <delfa07@bigmir.net>
2020-05-06 19:56:28 +0300

    33. all leagues (logos)

--- src/constants/leagues.js
+++ src/constants/leagues.js
@@ -1,55 +1,57 @@
+const logo = 'https://www.flashscore.com/res/image/data/';
+
 export const LEAGUES = {
   // soccer
   UEFA_CL: {
     TITLE: 'UEFA Champions League',
-    LOGO: 'https://www.flashscore.com/res/image/data/fBGAS29j-lQma9IdL.png',
+    LOGO: `${logo}fBGAS29j-lQma9IdL.png`,
     LINK: 'https://en.wikipedia.org/wiki/UEFA_Champions_League'
   },
   UEFA_EL: {
     TITLE: 'UEFA Europa League',
-    LOGO: 'https://www.flashscore.com/res/image/data/I11FocYI-GlDRoWIh.png',
+    LOGO: `${logo}I11FocYI-GlDRoWIh.png`,
     LINK: 'https://en.wikipedia.org/wiki/UEFA_Europa_League'
   },
   CONMEBOL_CL: {
     TITLE: 'CONMEBOL Copa Libertadores',
-    LOGO: 'https://www.flashscore.com/res/image/data/xEh5yUXI-WlmljIGa.png',
+    LOGO: `${logo}xEh5yUXI-WlmljIGa.png`,
     LINK: 'https://en.wikipedia.org/wiki/Copa_Libertadores'
   },
   CONCACAF_CL: {
     TITLE: 'CONCACAF Champions League',
-    LOGO: 'https://www.flashscore.com/res/image/data/U3aJXkEO-C61Zz1Ok.png',
+    LOGO: `${logo}U3aJXkEO-C61Zz1Ok.png`,
     LINK: 'https://en.wikipedia.org/wiki/CONCACAF_Champions_League'
   },
   MLS: {
     TITLE: 'Major League Soccer',
-    LOGO: 'https://www.flashscore.com/res/image/data/WlePe6U1-IBnJkea7.png',
+    LOGO: `${logo}WlePe6U1-IBnJkea7.png`,
     LINK: 'https://en.wikipedia.org/wiki/Major_League_Soccer'
   },
   CAF_CL: {
     TITLE: 'CAF Champions League',
-    LOGO: 'https://www.flashscore.com/res/image/data/xEpkaTiU-OdDPvzsD.png',
+    LOGO: `${logo}xEpkaTiU-OdDPvzsD.png`,
     LINK: 'https://en.wikipedia.org/wiki/CAF_Champions_League'
   },
   AFC_CL: {
     TITLE: 'AFC Champions League',
-    LOGO: 'https://www.flashscore.com/res/image/data/2qMu1z9j-th4rZfUn.png',
+    LOGO: `${logo}2qMu1z9j-th4rZfUn.png`,
     LINK: 'https://en.wikipedia.org/wiki/AFC_Champions_League'
   },
   OFC_CL: {
     TITLE: 'OFC Champions League',
-    LOGO: 'https://www.flashscore.com/res/image/data/2NGbBFQp-fcD6cba5.png',
+    LOGO: `${logo}2NGbBFQp-fcD6cba5.png`,
     LINK: 'https://en.wikipedia.org/wiki/OFC_Champions_League'
   },
 
   // basketball
   NBA: {
     TITLE: 'National Basketball Association',
-    LOGO: 'https://www.flashscore.com/res/image/data/UJuqUBEO-SQN4wn5S.png',
+    LOGO: `${logo}UJuqUBEO-SQN4wn5S.png`,
     LINK: 'https://en.wikipedia.org/wiki/National_Basketball_Association'
   },
   EURO_LEAGUE: {
     TITLE: 'EuroLeague',
-    LOGO: 'https://www.flashscore.com/res/image/data/bTE9GzCO-4UukMJVC.png',
+    LOGO: `${logo}bTE9GzCO-4UukMJVC.png`,
     LINK: 'https://en.wikipedia.org/wiki/EuroLeague'
   },
   BAL: {
@@ -59,110 +61,115 @@ export const LEAGUES = {
   },
   FIBA_ASIA: {
     TITLE: 'FIBA Asia Champions Cup',
-    LOGO: 'https://www.flashscore.com/res/image/data/ji1ZSCld-WU8K6JBo.png',
+    LOGO: `${logo}ji1ZSCld-WU8K6JBo.png`,
     LINK: 'https://en.wikipedia.org/wiki/FIBA_Asia_Champions_Cup'
   },
   ASEAN_BL: {
     TITLE: 'ASEAN Basketball League',
-    LOGO: 'https://www.flashscore.com/res/image/data/KdhJ9nB7-OAxu4Y0P.png',
+    LOGO: `${logo}KdhJ9nB7-OAxu4Y0P.png`,
     LINK: 'https://en.wikipedia.org/wiki/ASEAN_Basketball_League'
   },
   NBL: {
     TITLE: 'National Basketball League',
-    LOGO: 'https://www.flashscore.com/res/image/data/8hDU9E97-d0xosfWP.png',
+    LOGO: `${logo}8hDU9E97-d0xosfWP.png`,
     LINK: 'https://en.wikipedia.org/wiki/National_Basketball_League_(Australia)'
   },
   LDA: {
     TITLE: 'FIBA Americas League',
-    LOGO: 'https://www.flashscore.com/res/image/data/tjqgzATp-4rP2RuDC.png',
+    LOGO: `${logo}tjqgzATp-4rP2RuDC.png`,
     LINK: 'https://en.wikipedia.org/wiki/FIBA_Americas_League'
   },
 
   // ice hockey
   NHL: {
     TITLE: 'National Hockey League',
-    LOGO: 'https://www.flashscore.com/res/image/data/xlRuQfhD-d8noefNR.png',
+    LOGO: `${logo}xlRuQfhD-d8noefNR.png`,
     LINK: 'https://en.wikipedia.org/wiki/National_Hockey_League'
   },
   CHL: {
     TITLE: 'Champions Hockey League',
-    LOGO: 'https://www.flashscore.com/res/image/data/fsPiS59j-hrADtmUL.png',
+    LOGO: `${logo}fsPiS59j-hrADtmUL.png`,
     LINK: 'https://en.wikipedia.org/wiki/Champions_Hockey_League'
+  },
+  IIHF_CUP: {
+    TITLE: 'IIHF Continental Cup',
+    LOGO: `${logo}0YUE9ufU-plyKzx9A.png`,
+    LINK: 'https://en.wikipedia.org/wiki/IIHF_Continental_Cup'
   },
   KHL: {
     TITLE: 'Kontinental Hockey League',
-    LOGO: 'https://www.flashscore.com/res/image/data/zDv6cBiU-dpvIrs7K.png',
+    LOGO: `${logo}zDv6cBiU-dpvIrs7K.png`,
     LINK: 'https://en.wikipedia.org/wiki/Kontinental_Hockey_League'
   },
   ALIH: {
     TITLE: 'Asia League Ice Hockey',
-    LOGO: 'https://www.flashscore.com/res/image/data/jkSOZrV1-IkDjADlh.png',
+    LOGO: `${logo}jkSOZrV1-IkDjADlh.png`,
     LINK: 'https://en.wikipedia.org/wiki/Asia_League_Ice_Hockey'
   },
 
   // baseball
   MLB: {
     TITLE: 'Major League Baseball',
-    LOGO: 'https://www.flashscore.com/res/image/data/vejtzfhU-CdjtEF4s.png',
+    LOGO: `${logo}vejtzfhU-CdjtEF4s.png`,
     LINK: 'https://en.wikipedia.org/wiki/Major_League_Baseball'
   },
   CARIBBEAN_SER: {
     TITLE: 'Caribbean Series',
-    LOGO: 'https://www.flashscore.com/res/image/data/6HzgKX97-jPI9GoEM.png',
+    LOGO: `${logo}6HzgKX97-jPI9GoEM.png`,
     LINK: 'https://en.wikipedia.org/wiki/Caribbean_Series'
   },
   NPB: {
     TITLE: 'Nippon Professional Baseball',
-    LOGO: 'https://www.flashscore.com/res/image/data/GUAlcVT1-nFhzMryo.png',
+    LOGO: `${logo}GUAlcVT1-nFhzMryo.png`,
     LINK: 'https://en.wikipedia.org/wiki/Nippon_Professional_Baseball'
   },
   KBO: {
     TITLE: 'KBO League',
-    LOGO: 'https://www.flashscore.com/res/image/data/OC5VFTDO-WWHV9ksS.png',
+    LOGO: `${logo}OC5VFTDO-WWHV9ksS.png`,
     LINK: 'https://en.wikipedia.org/wiki/KBO_League'
   },
   CPBL: {
     TITLE: 'Chinese Professional Baseball League',
-    LOGO: 'https://www.flashscore.com/res/image/data/0zn815BO-nXZaM9MK.png',
+    LOGO: `${logo}0zn815BO-nXZaM9MK.png`,
     LINK: 'https://en.wikipedia.org/wiki/Chinese_Professional_Baseball_League'
   },
   ABL: {
     TITLE: 'Australian Baseball League',
-    LOGO: 'https://www.flashscore.com/res/image/data/no5DPmDO-6kYsyQM8.png',
+    LOGO: `${logo}no5DPmDO-6kYsyQM8.png`,
     LINK: 'https://en.wikipedia.org/wiki/Australian_Baseball_League'
   },
   ECC: {
     TITLE: 'European Champions Cup',
-    LOGO: 'https://www.flashscore.com/res/image/data/AVMVhTCO-MojGuvQ1.png',
+    LOGO: `${logo}AVMVhTCO-MojGuvQ1.png`,
     LINK: 'https://en.wikipedia.org/wiki/European_Cup_(baseball)'
   },
 
   // football
   NFL: {
     TITLE: 'National Football League',
-    LOGO: 'https://www.flashscore.com/res/image/data/8AA2C9jD-jqqU0YfF.png',
+    LOGO: `${logo}8AA2C9jD-jqqU0YfF.png`,
     LINK: ''
   },
   CFL: {
     TITLE: 'Canadian Football League',
-    LOGO: 'https://www.flashscore.com/res/image/data/tU1ZcI9j-AJ2C7kM9.png',
+    LOGO: `${logo}tU1ZcI9j-AJ2C7kM9.png`,
     LINK: ''
   },
   AFL: {
     TITLE: 'Australian Football League',
-    LOGO: 'https://www.flashscore.com/res/image/data/K00W62V1-ARWG46at.png',
+    LOGO: `${logo}K00W62V1-ARWG46at.png`,
     LINK: ''
   },
   X_LEAGUE: {
     TITLE: 'X-League',
-    LOGO: 'https://www.flashscore.com/res/image/data/bcAarMU1-rgjN0MlN.png',
+    LOGO: `${logo}bcAarMU1-rgjN0MlN.png`,
     LINK: 'https://en.wikipedia.org/wiki/X-League'
   },
 
   // rugby union
   SUPER_RUGBY: {
     TITLE: 'Super Rugby',
-    LOGO: 'https://www.flashscore.com/res/image/data/Ucd0OR97-l0bUvsur.png',
+    LOGO: `${logo}Ucd0OR97-l0bUvsur.png`,
     LINK: 'https://en.wikipedia.org/wiki/Super_Rugby'
   },
   GRR: {
@@ -172,71 +179,71 @@ export const LEAGUES = {
   },
   EUROPEAN_RUGBY_CC: {
     TITLE: 'European Rugby Champions Cup',
-    LOGO: 'https://www.flashscore.com/res/image/data/Eqk6KZSp-YkXrNykG.png',
+    LOGO: `${logo}Eqk6KZSp-YkXrNykG.png`,
     LINK: 'https://en.wikipedia.org/wiki/European_Rugby_Champions_Cup'
   },
   PRO_14: {
     TITLE: 'Guinness PRO14 Championship',
-    LOGO: 'https://www.flashscore.com/res/image/data/KAFW9qhD-j9MAT9V0.png',
+    LOGO: `${logo}KAFW9qhD-j9MAT9V0.png`,
     LINK: 'https://en.wikipedia.org/wiki/Pro14'
   },
   SLAR: {
     TITLE: 'Superliga Americana de Rugby',
-    LOGO: 'https://www.flashscore.com/res/image/data/n9951l9j-ULMtnkLN.png',
+    LOGO: `${logo}n9951l9j-ULMtnkLN.png`,
     LINK: 'https://en.wikipedia.org/wiki/S%C3%BAper_Liga_Americana_de_Rugby'
   },
   MLR: {
     TITLE: 'Major League Rugby',
-    LOGO: 'https://www.flashscore.com/res/image/data/hOR3X9B7-jTGgCT7B.png',
+    LOGO: `${logo}hOR3X9B7-jTGgCT7B.png`,
     LINK: 'https://en.wikipedia.org/wiki/Major_League_Rugby'
   },
 
   // rugby league
   NRL: {
     TITLE: 'National Rugby League',
-    LOGO: 'https://www.flashscore.com/res/image/data/fP3FFMhU-0lik8oz7.png',
+    LOGO: `${logo}fP3FFMhU-0lik8oz7.png`,
     LINK: 'https://en.wikipedia.org/wiki/National_Rugby_League'
   },
   SUPER_LEAGUE: {
     TITLE: 'Super League',
-    LOGO: 'https://www.flashscore.com/res/image/data/vNS4O4hU-jVFYBRIh.png',
+    LOGO: `${logo}vNS4O4hU-jVFYBRIh.png`,
     LINK: 'https://en.wikipedia.org/wiki/Super_League'
   },
   ELITE_ONE: {
     TITLE: 'Elite One Championship',
-    LOGO: 'https://www.flashscore.com/res/image/data/bu3ZwOld-QNfXTeTP.png',
+    LOGO: `${logo}bu3ZwOld-QNfXTeTP.png`,
     LINK: 'https://en.wikipedia.org/wiki/Elite_One_Championship'
   },
 
   // cricket t20
   IPL: {
     TITLE: 'Indian Premier League',
-    LOGO: 'https://www.flashscore.com/res/image/data/GSmhyH7j-QaC2x75D.png',
+    LOGO: `${logo}GSmhyH7j-QaC2x75D.png`,
     LINK: 'https://en.wikipedia.org/wiki/Indian_Premier_League'
   },
   BBL: {
     TITLE: 'Big Bash League',
-    LOGO: 'https://www.flashscore.com/res/image/data/xrQJFsRp-fyEl1Ydj.png',
+    LOGO: `${logo}xrQJFsRp-fyEl1Ydj.png`,
     LINK: 'https://en.wikipedia.org/wiki/Big_Bash_League'
   },
   T20_BLAST: {
     TITLE: 'T20 Blast',
-    LOGO: 'https://www.flashscore.com/res/image/data/0jEhEajD-Ecvvdfrh.png',
+    LOGO: `${logo}0jEhEajD-Ecvvdfrh.png`,
     LINK: 'https://en.wikipedia.org/wiki/Twenty20_Cup'
   },
   BPL: {
     TITLE: 'Bangladesh Premier League',
-    LOGO: 'https://www.flashscore.com/res/image/data/AsXhAKiD-MuZLL4aN.png',
+    LOGO: `${logo}AsXhAKiD-MuZLL4aN.png`,
     LINK: 'https://en.wikipedia.org/wiki/Bangladesh_Premier_League'
   },
   PSL: {
     TITLE: 'Pakistan Super League',
-    LOGO: 'https://www.flashscore.com/res/image/data/QaK95BB7-Mm9UrSTm.png',
+    LOGO: `${logo}QaK95BB7-Mm9UrSTm.png`,
     LINK: 'https://en.wikipedia.org/wiki/Pakistan_Super_League'
   },
   CPL: {
     TITLE: 'Caribbean Premier League',
-    LOGO: 'https://www.flashscore.com/res/image/data/d6meaKiD-rq38UHtl.png',
+    LOGO: `${logo}d6meaKiD-rq38UHtl.png`,
     LINK: 'https://en.wikipedia.org/wiki/Caribbean_Premier_League'
   }
 };
